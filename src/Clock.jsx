eed8e5773d4c95c7574508cc32dfd37f64923d35
salimtaiyo <ichigeki.ccCsc@hotmail.com>
2018-05-23 22:57:42 -0400

    added update functionality

--- src/Clock.jsx
+++ src/Clock.jsx
@@ -10,29 +10,43 @@ class Clock extends Component {
             hrs:0,
             min:0,
             sec: 0,
-            days:0
+        
         }
     }
 
+    componentWillMount(){
+        this.getTime(this.props.deadline);
+    }
+
+    componentDidMount(){
+       setInterval(() => this.getTime(this.props.deadline),1000);
+    }
+
+    
+
     getTime(deadline){
-        const time = Date.parse(deadline)- Date.parse(new Date());
+        const time = Date.parse(deadline) - Date.parse(new Date());
         const sec = Math.floor((time/1000)%60);
         const min = Math.floor((time/1000/60) % 60);
         const hrs = Math.floor(time/(1000*60*60) % 24);
         const days = Math.floor(time/(1000*60*60*24));
         
-        console.log(`days: ${days} hours ${hrs} mins ${min} sec ${sec}`);
+        this.setState({
+            days, hrs, min ,sec
+        })
     }
 
+    addingZero(x){
+       return  x < 10 ? '0' + x: x;
+    }
     render(){
-            this.getTime(this.props.deadline);
+        
         return(
             <div>
-                {this.props.deadline}
-                <div className="app__c">{this.state.days}</div>
-                <div className="app__c"> {this.state.hours}</div>
-                <div className="app__c"> {this.state.minute}</div>
-                <div className="app__c"> {this.state.sec}</div>
+                <div className="app__c">{this.addingZero(this.state.days)}</div>
+                <div className="app__c"> {this.addingZero(this.state.hrs)}</div>
+                <div className="app__c"> {this.addingZero(this.state.min)}</div>
+                <div className="app__c"> {this.addingZero(this.state.sec)}</div>
             </div>
         )
     }
