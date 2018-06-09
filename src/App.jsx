b1f06ae1861519b597a6d557f7804f7eb19d21e4
salimtaiyo <ichigeki.ccCsc@hotmail.com>
2018-06-08 23:25:53 -0400

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,76 @@
+// import React, { Component } from 'react';
+// import Clock from './Clock';
+
+// class App extends Component {
+//     constructor(){
+//         super()
+
+//         this.state = {
+//             deadline : 'December 25 2018',
+//             newDeadline:''
+//         }
+//     }
+
+//     changeHandler(){
+//         this.setState ( {
+//             deadline: this.state.newDeadline
+//         })
+//     }
+
+//     newDiv(event) {
+//         this.setState({
+//             newDeadline: event.target.value
+//         })
+//     }
+
+//     render() {
+//         return (
+//             <div className="app">
+//                 <h1> {this.state.deadline}</h1>
+//                 <Clock deadline={this.state.deadline}/>
+//                 <div> 
+//                     <input onChange={(event) => this.newDiv(event) } placeholder="input"/>
+//                     <button onClick={() => this.changeHandler()} type="submit"> submit</button>
+//                 </div>
+//             </div>
+//         )
+//     }
+// }
+
+// export default App;
 import React, { Component } from 'react';
 import Clock from './Clock';
 
-class App extends Component {
+class App extends Component{
     constructor(){
-        super()
-
-        this.state = {
-            deadline : 'December 25 2018',
-            newDeadline:''
+        super();
+        this.state={
+            deadline:'December 25 2018',
+            newDeadline: ''
         }
     }
 
-    changeHandler(){
-        this.setState ( {
-            deadline: this.state.newDeadline
-        })
-    }
-
-    newDiv(event) {
+    changeHandler= (event) => { 
         this.setState({
             newDeadline: event.target.value
         })
     }
 
-    render() {
+    newValue = () => {
+        this.setState({
+            deadline: this.state.newDeadline
+        })
+    }
+
+    render(){
         return (
-            <div className="app">
+            <div> 
                 <h1> {this.state.deadline}</h1>
                 <Clock deadline={this.state.deadline}/>
-                <div> 
-                    <input onChange={(event) => this.newDiv(event) } placeholder="input"/>
-                    <button onClick={() => this.changeHandler()} type="submit"> submit</button>
-                </div>
+                <input onChange={(event) => this.changeHandler(event)} placeholder="Set a deadline"/>
+                <button onClick={() => this.newValue()}> Save </button> 
             </div>
         )
-        
-        
-       
     }
 }
 
--- src/Clock.jsx
+++ src/Clock.jsx
@@ -1,53 +1,72 @@
-import React, { Component } from 'react';
+// import React, { Component } from 'react';
 
 
-class Clock extends Component {
-    constructor(props){
-        super(props);
+// class Clock extends Component {
+//     constructor(props){
+//         super(props);
 
-        this.state = {
-            days: 0,
-            hrs:0,
-            min:0,
-            sec: 0,
+//         this.state = {
+//             days: 0,
+//             hrs:0,
+//             min:0,
+//             sec: 0,
         
-        }
-    }
+//         }
+//     }
 
-    componentWillMount(){
-        this.getTime(this.props.deadline);
-    }
+//     componentWillMount(){
+//         this.getTime(this.props.deadline);
+//     }
 
-    componentDidMount(){
-       setInterval(() => this.getTime(this.props.deadline),1000);
-    }
+//     componentDidMount(){
+//        setInterval(() => this.getTime(this.props.deadline),1000);
+//     }
 
     
 
-    getTime(deadline){
-        const time = Date.parse(deadline) - Date.parse(new Date());
-        const sec = Math.floor((time/1000)%60);
-        const min = Math.floor((time/1000/60) % 60);
-        const hrs = Math.floor(time/(1000*60*60) % 24);
-        const days = Math.floor(time/(1000*60*60*24));
+//     getTime(deadline){
+//         const time = Date.parse(deadline) - Date.parse(new Date());
+//         const sec = Math.floor((time/1000)%60);
+//         const min = Math.floor((time/1000/60) % 60);
+//         const hrs = Math.floor(time/(1000*60*60) % 24);
+//         const days = Math.floor(time/(1000*60*60*24));
         
-        this.setState({
-            days, hrs, min ,sec
-        })
+//         this.setState({
+//             days, hrs, min ,sec
+//         })
+//     }
+
+//     addingZero(x){
+//        return  x < 10 ? '0' + x: x;
+//     }
+//     render(){
+        
+//         return(
+//             <div>
+//                 <div className="app__c">{this.addingZero(this.state.days)}</div>
+//                 <div className="app__c"> {this.addingZero(this.state.hrs)}</div>
+//                 <div className="app__c"> {this.addingZero(this.state.min)}</div>
+//                 <div className="app__c"> {this.addingZero(this.state.sec)}</div>
+//             </div>
+//         )
+//     }
+// }
+
+// export default Clock;
+import React, { Components } from 'react';
+import App from './App';
+
+class Clock extends Components{
+    constructor(props){
+        super(props);
+            this.state = {
+                time: ''
+            }
     }
 
-    addingZero(x){
-       return  x < 10 ? '0' + x: x;
-    }
     render(){
-        
         return(
-            <div>
-                <div className="app__c">{this.addingZero(this.state.days)}</div>
-                <div className="app__c"> {this.addingZero(this.state.hrs)}</div>
-                <div className="app__c"> {this.addingZero(this.state.min)}</div>
-                <div className="app__c"> {this.addingZero(this.state.sec)}</div>
-            </div>
+            <h1> ejs </h1>
         )
     }
 }
